ac35bfc59c448f6c8d744271862686a858cfecc3
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-22 09:35:27 +0300

    Connect index template and New Question Component Rendering New Question Component in index.hbs Create saveQuestion action to take in user form input and add and save it in the firebase database

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('question')
+    }, 
+    actions: {
+        // Create and save the new question
+        saveQuestion(params) {
+            // Create a new question object with params gotten from the form
+            var newQuestion = this.store.createRecord('question', params);
+            // Save the new question in the database
+            newQuestion.save();
+            // Reload the index page
+            this.transitionTo('index');
+        }
     }
 });
