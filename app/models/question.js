93d76aa6731692478ce98b204310589b47ae2aa1
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-22 12:15:16 +0300

    Create one-to-many relationship Create one-to-many relationship between Question Model and Answer Model

--- app/models/question.js
+++ app/models/question.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
     questionTitle: DS.attr(),
     authorQuestion: DS.attr(),
     contentQuestion: DS.attr(),
+    answers: DS.hasMany('answer', {async: true})
 });
