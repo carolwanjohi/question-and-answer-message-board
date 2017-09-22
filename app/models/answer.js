adf8162f7b9aac7c74d31905751129b4b281cb3d
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-22 12:11:29 +0300

    Create Answer Model Create Answer Model for answers provided by the user

---
+++ app/models/answer.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    authorAnswer: DS.attr(),
+    contentAnswer: DS.attr(),
+    question: DS.belongsTo('question', {async: true})
+
+});
