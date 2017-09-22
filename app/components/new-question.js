9da93574211d0570f7cb2245ea5bfa5ecac57725
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-22 08:35:04 +0300

    Create New Question Component Create New Question Component to display a button for creating a Question form when clicked Create showQuestionForm and saveQuestion actions

---
+++ app/components/new-question.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    // Hide the form
+    addNewQuestion: false,
+
+    actions: {
+        // Show the form
+        showQuestionForm() {
+            this.set('addNewQuestion', true)
+        },
+
+        saveQuestion() {
+            // Get the user input and store them
+            var params = {
+                authorQuestion: this.get('authorQuestion'),
+                questionTitle: this.get('questionTitle'),
+                contentQuestion: this.get('contentQuestion')
+            };
+            // Hide the form
+            this.set('addNewQuestion', false);
+
+            // Send action to route handler to create and save the new question
+            this.sendAction('saveQuestion', params);
+        },
+    }
+});
