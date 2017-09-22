d40d7ee1373f15703e352060e4860e6354f134ec
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-22 07:34:21 +0300

    Update files Change title in index.html to match name of the site Change layout of applications.hbs so that all the pages have a header and footer Change name of an attribute in the question model and questions seed database file

--- app/models/question.js
+++ app/models/question.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    question: DS.attr();
-    authorQuestion: DS.attr();
-    contentQuestion: DS.attr();
+    questionTitle: DS.attr(),
+    authorQuestion: DS.attr(),
+    contentQuestion: DS.attr(),
 });
