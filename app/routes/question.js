4372053dec9b70904f6e5ca9fdf2aaf3b72aecef
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-22 11:55:32 +0300

    Connect Edit Question Component and Question Route Handler Render Edit Question Component and its edit action in question.hbs Edit the old question details with the new input values in the firebase database and reload the question page

--- app/routes/question.js
+++ app/routes/question.js
@@ -6,6 +6,23 @@ export default Ember.Route.extend({
     },
 
     actions: {
-        
+        // Edit an existing question
+        edit(question, params) {
+
+            // Get the use input and loop through it
+            Object.keys(params).forEach(function(key) {
+                // If the user has changed any detail replace the old infomation with the new information
+                if(params[key]!==undefined) {
+                question.set(key,params[key]);
+                }
+            });
+
+            // Save the new changes
+            question.save();
+
+            // Reload the question page
+            this.transitionTo('question', question);
+
+        }
     }
 });
