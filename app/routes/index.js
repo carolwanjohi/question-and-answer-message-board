13b43b5155f3399024875d33481b8de2780b9f30
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-22 18:16:47 +0300

    Move functions Move saveAnswer() function from Index Route Handler to Question Route Handler

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.store.findAll('question')
-    }, 
+        return Ember.RSVP.hash({
+            questions: this.store.findAll('question'),
+            answers: this.store.findAll('answer')
+        });
+  }, 
     actions: {
         // Create and save the new question
         saveQuestion(params) {
@@ -13,6 +16,6 @@ export default Ember.Route.extend({
             newQuestion.save();
             // Reload the index page
             this.transitionTo('index');
-        }
+        },
     }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -11,7 +11,7 @@ export default Ember.Route.extend({
 
             // Get the use input and loop through it
             Object.keys(params).forEach(function(key) {
-                // If the user has changed any detail replace the old infomation with the new information
+                // If the user has changed any detail replace the old information with the new information
                 if(params[key]!==undefined) {
                 question.set(key,params[key]);
                 }
@@ -20,9 +20,34 @@ export default Ember.Route.extend({
             // Save the new changes
             question.save();
 
+            // Reload the index page
+            this.transitionTo('index');
+
+        },
+
+        // Create and save an answer for a specified question
+        saveAnswer(params) {
+            // Create a new answer object with params gotten from the form
+            var newAnswer = this.store.createRecord('answer', params);
+
+            // Get the current question and store it
+            var question = params.question;
+
+            // Go to the current question, get the answers parameter and store the new answer in the answers object
+            question.get('answers').addObject(newAnswer);
+
+            // Save the new answer then save the question with its new answers parameter
+            newAnswer.save().then( function() {
+                return question.save();
+            });
+
             // Reload the question page
             this.transitionTo('question', question);
 
         }
+
     }
 });
+
+
+
