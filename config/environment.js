4717bd891f78dd88a787da84c3c362d8958c1f00
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-22 06:48:18 +0300

    Create seed data, install emberfire and add configurations to firebase

--- config/environment.js
+++ config/environment.js
@@ -18,6 +18,15 @@ module.exports = function(environment) {
       }
     },
 
+    firebase: {
+      apiKey: "AIzaSyByAbo9PBvO2Fchx8A9PrwNIAM5-66H5Ng",
+      authDomain: "help-message-board.firebaseapp.com",
+      databaseURL: "https://help-message-board.firebaseio.com",
+      projectId: "help-message-board",
+      storageBucket: "help-message-board.appspot.com",
+      messagingSenderId: "752672336224"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
