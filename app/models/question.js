f244ae4f4830573b974d01e7b85234656a8ac7e1
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-29 08:17:39 +0300

    Create Computed Property Inject Question Cart service Create inCart computed property that implements the includes function and checks if a question is already in the list of favorite questions

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,14 @@ export default DS.Model.extend({
     questionTitle: DS.attr(),
     authorQuestion: DS.attr(),
     contentQuestion: DS.attr(),
-    answers: DS.hasMany('answer', { async: true })
+    answers: DS.hasMany('answer', { async: true }),
+
+    // Inject question cart 
+    questionCart: Ember.inject.service(),
+
+    // Computed property to check if a question is already in the question cart
+    inCart : Ember.computed('questionCart.questions.[]', function() {
+        return this.get('questionCart').includes(this);
+    })
+
 });
