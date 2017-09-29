4446c4c1cd8a43b53d22ddbe1edefe935358541d
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-29 06:44:24 +0300

    Create Add To Favorite Question Button Create Add To Favorite Question button to add a specific question to the list of favorite questions on Index Template Create addToQuestionCart function which adds a specific question to the list of favorite questions on the Index Template Inject Question Cart Service to eneble addToQuestionCart function to work

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    questionCart: Ember.inject.service(),
+
+    actions: {
+        addToQuestionCart(question) {
+            this.get('questionCart').add(question);
+        }
+    }
 });
