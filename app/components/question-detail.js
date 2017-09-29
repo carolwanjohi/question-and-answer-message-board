35742d5ff40459c911d3e050cd771945cac14b2d
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-29 13:02:38 +0300

    Create Answers Count Computed Property Create Computed Property answersCount to show the number of answers a question has Render answersCount Computed Property in Question Detail Component and Question Tile Component

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -5,4 +5,8 @@ export default Ember.Component.extend({
     heading: Ember.computed('question.questionTitle', 'question.authorQuestion', function() {
         return this.get('question.questionTitle') + ' by ' + this.get('question.authorQuestion');
     }),
+
+    answersCount: Ember.computed('question.answers.length', function() {
+        return this.get('question.answers.length') + ' answers'
+    })
 });
