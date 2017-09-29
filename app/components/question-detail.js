d1ac8256463c73cb962889d32b1a6a7af54a9530
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-29 12:34:54 +0300

    Create Computed Property Heading Create Computed Property Heading to display the Question Title and Question Author properties Render Computed Property Heading in Question Detail Component Remove Question Title and Question Author Properties from Question Template

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    heading: Ember.computed('question.questionTitle', 'question.authorQuestion', function() {
+        return this.get('question.questionTitle') + ' by ' + this.get('question.authorQuestion');
+    }),
 });
