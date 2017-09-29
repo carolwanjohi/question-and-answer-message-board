237be502dd22f0c6e13f4707e9675e162af630dd
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-29 08:12:38 +0300

    Create includes function Create includes function that checks if a question is already in the questions array

--- app/services/question-cart.js
+++ app/services/question-cart.js
@@ -7,6 +7,11 @@ export default Ember.Service.extend({
     // Push new question to questions array
     add(question) {
         this.get('questions').pushObject(question);
+    },
+
+    // Check if a question is already included in the questions array
+    includes(question) {
+        return this.get('questions').includes(question)
     }
 
 });
